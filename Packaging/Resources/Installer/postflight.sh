80410ba1efd92ae5130124abc032006f2259ba99
pmd <github@girlintheshell.com>
2015-08-08 13:03:38 -0500

    updated packaging

--- Packaging/Resources/Installer/postflight.sh
+++ Packaging/Resources/Installer/postflight.sh
@@ -1,24 +1,30 @@
 #!/bin/sh
 
 echo "Load kernel extension"
-if [ -d "/System/Library/Extensions/Xbox HID.kext" ]; then
-	kextload "/System/Library/Extensions/Xbox HID.kext"
+if [ -d "/System/Library/Extensions/SylpHID.kext" ]; then
+	kextload "/System/Library/Extensions/SylpHID.kext"
 
-elif [ -d "/Library/Extensions/Xbox HID.kext" ]; then
-	kextload "/Library/Extensions/Xbox HID.kext"
+elif [ -d "/Library/Extensions/SylpHID.kext" ]; then
+	kextload "/Library/Extensions/SylpHID.kext"
 
 fi
 
 
 echo "Start background daemon"
-sudo -u $USER launchctl load /Library/LaunchAgents/com.fizzypopstudios.XboxHIDDaemon.plist
-sudo -u $USER launchctl start com.fizzypopstudios.XboxHIDDaemon
+sudo -u $USER launchctl load /Library/LaunchAgents/com.fizzypopstudios.SylpHIDDaemon.plist
+sudo -u $USER launchctl start com.fizzypopstudios.SylpHIDDaemon
 
 
 echo "Relaunch System Preferences"
 PROCESS="System Preferences"
 number=$(ps aux | grep -i "$PROCESS" | grep -v grep | wc -l)
-if [ $number -gt 0 ]
-    then
-        killall "$PROCESS" && sudo -u $USER open -g "/Applications/System Preferences.app"
+if [ $number -gt 0 ]; then
+	killall "$PROCESS"
+	touch "/tmp/sylphid.sysprefs"
+fi
+
+
+if [ -f "/tmp/sylphid.sysprefs" ]; then
+	rm -f "/tmp/sylphid.sysprefs"
+	sudo -u $USER open -g "/Applications/System Preferences.app"
 fi
--- Packaging/Resources/Installer/preflight.sh
+++ Packaging/Resources/Installer/preflight.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+syspref=0
+
 echo "Checking for 'xhd' driver"
 if [ -d /System/Library/Extensions/DWXboxHIDDriver.kext ]; then
 	echo "Removing old 'xhd' driver"
@@ -25,31 +27,44 @@ if [ -d /System/Library/Extensions/DWXboxHIDDriver.kext ]; then
 	rm -rf /private/var/db/receipts/org.walisser.driver.DWXBoxHIDDriver.bom
 	rm -rf /private/var/db/receipts/org.walisser.driver.DWXBoxHIDDriver.plist
 
-	echo "Relaunch System Preferences"
 	PROCESS="System Preferences"
-	number=$(ps aux | grep -i "$PROCESS" | grep -v grep | wc -l)
-	if [ $number -gt 0 ]
-		then
-			killall "$PROCESS" && open -g "/Applications/System Preferences.app"
+	syspref=$(ps aux | grep -i "$PROCESS" | grep -v grep | wc -l)
+	if [ $syspref -gt 0 ]; then
+		killall "$PROCESS"
 	fi
 fi
 
 
 echo "Checking if upgrading"
-if [ -d "/Library/PreferencePanes/Xbox HID.prefPane" ]; then
+if [ -d "/Library/PreferencePanes/SylpHID.prefPane" ]; then
 	echo "Removing previous version"
 
-	sudo -u $USER launchctl stop com.fizzypopstudios.XboxHIDDaemon
-	sudo -u $USER launchctl unload /Library/LaunchAgents/com.fizzypopstudios.XboxHIDDaemon.plist
-	rm /Library/LaunchAgents/com.fizzypopstudios.XboxHIDDaemon.plist
+	sudo -u $USER launchctl stop com.fizzypopstudios.SylpHIDDaemon
+	sudo -u $USER launchctl unload /Library/LaunchAgents/com.fizzypopstudios.SylpHIDDaemon.plist
+	rm /Library/LaunchAgents/com.fizzypopstudios.SylpHIDDaemon.plist
 
-	if [ -d "/System/Library/Extensions/Xbox HID.kext" ]; then
-		kextunload "/System/Library/Extensions/Xbox HID.kext"
-		rm -rf "/System/Library/Extensions/Xbox HID.kext"
+	rm -rf "/Library/PreferencePanes/SylpHID.prefPane"
 
-	elif [ -d "/Library/Extensions/Xbox HID.kext" ]; then
-		kextunload "/Library/Extensions/Xbox HID.kext"
-		rm -rf "/Library/Extensions/Xbox HID.kext"
+	if [ -d "/System/Library/Extensions/SylpHID.kext" ]; then
+		kextunload "/System/Library/Extensions/SylpHID.kext"
+		rm -rf "/System/Library/Extensions/SylpHID.kext"
+
+	elif [ -d "/Library/Extensions/SylpHID.kext" ]; then
+		kextunload "/Library/Extensions/SylpHID.kext"
+		rm -rf "/Library/Extensions/SylpHID.kext"
 
 	fi
+
+	if [ $syspref -eq 0 ]; then
+		PROCESS="System Preferences"
+		syspref=$(ps aux | grep -i "$PROCESS" | grep -v grep | wc -l)
+		if [ $syspref -gt 0 ]; then
+			killall "$PROCESS"
+		fi
+	fi
+fi
+
+
+if [ $syspref -gt 0 ]; then
+	touch /tmp/sylphid.sysprefs
 fi
